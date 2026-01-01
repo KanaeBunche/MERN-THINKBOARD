000b16c406af4b158081e0000a652f9e5c2862e0
Kanae Bunche <kanaebunche@Kanaes-iMac.local>
2026-01-01 16:27:25 -0500

    ADDED routes and server.js

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,25 @@ import express from 'express';
 
 const app = express();
 
-// Build a route 
+
+
+// Build a route //def routes 
 app.get("/api/notes", (req, res) => {
-    res.send("You got 5 notes")
+    res.status(200).send("You got 10 notes")
 });
+
+app.post("/api/notes", (req, res) => {
+    res.status(201).json({message:"post created successfully!"})
+});
+
+app.put("/api/notes:id", (req, res) => {
+    res.status(200).json({message:"post updated successfully!"})
+});
+
+app.delete("/api/notes:id", (req, res) => {
+    res.status(200).json({message:"notes deleted successfully!"})
+});
+
 
 app.listen(50001, () => {
     console.log('Server is running on port 50001');
